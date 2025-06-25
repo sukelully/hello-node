a416591ae56ffe63ce7dababe3522f991930bd9c
Luke Sullivan <luke@sukelully.dev>
2025-06-25 12:08:33 +0100

    add eventEmitter.js

---
+++ src/eventEmitter.js
@@ -0,0 +1,5 @@
+const EventEmitter = require('node:events');
+const eventEmitter = new EventEmitter();
+
+eventEmitter.on('start', number => console.log(`started ${number}`));
+eventEmitter.emit('start', 23);
